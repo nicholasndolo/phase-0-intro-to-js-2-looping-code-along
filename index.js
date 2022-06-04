83cdd7117a2746f7823de0d249e6e562f7a64e31
Nicholas <nicholas.nmalinda@gmail.com>
2022-06-04 16:55:03 +0300

    Lab complete

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+const classMates = ["Guadalupe", "Ollie", "Aki"];
+const message = [];
+function writeCards(array, event){
+  for (let i = 0; i < array.length; i++)
+  message.push(`Thank you, ${array[i]}, for the wonderful surprise gift!`)
+  return message;
+}
+
+function countDown(number) {
+  let myNumber = number;
+  while(myNumber > -1) {
+    console.log(myNumber);
+    myNumber--;
+  }
+}
+
